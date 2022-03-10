c1dddfc8f54e733d815691b6dc361e56b2a51159
Jose Mejia <mejialaguna@yahoo.com>
2022-03-09 19:59:46 -0500

    meta-data

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,33 @@
 import { magicAdmin } from "../../lib/magic-Server";
+import jwt from "jsonwebtoken";
 
 async function login(req, res) {
   if (req.method === "POST") {
     try {
       const auth = req.headers.authorization;
-        const didToken = auth ? auth.substring(7) : "";
-        console.log({ didToken });
+      const didToken = auth ? auth.substring(7) : "";
+
       const metadata = await magicAdmin.users.getMetadataByToken(didToken);
-      console.log({metadata})
-      res.send({ message: "hello world 1" });
+      console.log({ metadata });
+      const token = jwt.sign(
+        {
+          ...metadata,
+          iat: Math.floor(Date.now() / 1000),
+          exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+          "https://hasura.io/jwt/claims": {
+            "x-hasura-allowed-roles": ["user", "admin"],
+            "x-hasura-default-role": "user",
+            "x-hasura-user-id": `${metadata.issuer}`,
+          },
+        },
+        `${process.env.jwt_secret_key}`
+      );
+console.log({token})
+      res.send({
+        message: { metadata },
+        didToken: didToken,
+        token,
+      });
     } catch (err) {
       console.error("something went wrong", err);
       res.status(500);
