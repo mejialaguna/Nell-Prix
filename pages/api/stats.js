b0208076c1a8860ed107b9e8d0b13373fffdd827
Jose Mejia <mejialaguna@yahoo.com>
2022-03-22 13:16:08 -0400

    insert-stats

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,32 +1,46 @@
 var jwt = require("jsonwebtoken");
-import { findVideoIdByUser } from "../../lib/db/index";
+import { findVideoIdByUser, updateStats, insertStats } from "../../lib/db";
 
 async function stats(req, res) {
-  if (req.method === "POST") {
-    try {
+  try {
+    if (req.method === "POST") {
       const token = req.cookies.token;
       console.log("line 7 ---------", { token });
-        if (token) {
+      if (token) {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const userId = decoded.issuer;
         const videoId = req.query.videoId;
-        const findVideo = await findVideoIdByUser(token, userId, videoId);
-        
-        if(findVideo){
-            
+          const findVideo = await findVideoIdByUser(token, userId, videoId);
+          const videoExist = findVideo?.length > 0;
+        console.log({ findVideo });
+        if (videoExist) {
+          const response = await updateStats(token, {
+            favorite: 1,
+            userId,
+            watched: false,
+            videoId: "mYfJxlgR2jw",
+          });
+          //   console.log({ response });
+          res.send({ message: "working update stats", response });
         } else {
-            
+          const response = await insertStats(token, {
+            watched: false,
+            userId,
+            videoId,
+            favorite: 10,
+          });
+          console.log({ response });
+          res.send({ message: "working insert stats", response });
         }
-        res.send({ message: "working", findVideo, decoded });
       } else {
         res.status(403);
         res.send({ message: "not working" });
       }
-    } catch (error) {
-      console.error({ message: "error has ocurred /stats", error });
-      res.status(500).send({ done: false, error: error.message });
-      console.log({ error });
     }
+  } catch (error) {
+    console.error({ message: "error has ocurred /stats", error });
+    res.status(500).send({ done: false, error: error.message });
+    console.log({ error });
   }
 }
 
