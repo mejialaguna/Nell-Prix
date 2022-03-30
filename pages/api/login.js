8a16e2bcf647ea6c274772323c9cdf5d728aebbd
Jose Mejia <mejialaguna@yahoo.com>
2022-03-30 11:55:48 -0400

    config

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,4 @@
-import { magicAdmin } from "../../lib/magic-Server";
+import {magicAdmin} from "../../lib/magic-Server";
 import jwt from "jsonwebtoken";
 import { isNewUser, createNewUser } from "../../lib/db";
 import { setTokenCookie } from "../../lib/cookie";
@@ -9,34 +9,28 @@ export default async function login(req, res) {
   if (req.method === "POST") {
     try {
       const auth = req.headers.authorization;
-      const DIDToken = auth ? auth.substr(7) : "";
-      
-      const metadata = await magicAdmin.users.getMetadataByToken(DIDToken);
-      const { issuer, email, publicAddress } = metadata;
+      const didToken = auth ? auth.substr(7) : "";
+
+      const metadata = await magicAdmin.users.getMetadataByToken(didToken);
 
       const token = jwt.sign(
         {
-          issuer,
-          email,
-          publicAddress,
+          ...metadata,
           iat: Math.floor(Date.now() / 1000),
           exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
           "https://hasura.io/jwt/claims": {
             "x-hasura-allowed-roles": ["user", "admin"],
             "x-hasura-default-role": "user",
-            "x-hasura-user-id": `${issuer}`,
+            "x-hasura-user-id": `${metadata.issuer}`,
           },
         },
         process.env.JWT_SECRET
       );
-     
 
-      const isNewUserQuery = await isNewUser(token, issuer);
+      const isNewUserQuery = await isNewUser(token, metadata.issuer);
       isNewUserQuery && (await createNewUser(token, metadata));
-      setTokenCookie(token, res)   
-      
+      setTokenCookie(token, res);
       res.send({ done: true });
-      
     } catch (error) {
       console.error("Something went wrong logging in", error);
       res.status(500).send({ done: false });
