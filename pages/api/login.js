4e4967419d5afdf1263fc8f42c5de36cd2bc6928
Jose Mejia <mejialaguna@yahoo.com>
2022-03-31 10:35:39 -0400

    m-admin

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,4 @@
-import { magicAdmin } from "../../lib/magic-Server";
+import {mAdmin} from "../../lib/magic-Server"
 import jwt from "jsonwebtoken";
 import { isNewUser, createNewUser } from "../../lib/db";
 import { setTokenCookie } from "../../lib/cookie";
@@ -7,28 +7,32 @@ export default async function login(req, res) {
   if (req.method === "POST") {
     try {
       const auth = req.headers.authorization;
-      const didToken = auth ? auth.substr(7) : "";
-      console.log({didToken})
-      const metadata = await magicAdmin.users.getMetadataByToken(didToken);
+      const DIDToken = auth ? auth.substr(7) : "";
+      console.log({DIDToken})
+      const metadata = await mAdmin.users.getMetadataByToken(DIDToken);
+      console.log({metadata})
       const { issuer, email, publicAddress } = metadata;
 
       const token = jwt.sign(
         {
-          ...metadata,
+          issuer,
+          email,
+          publicAddress,
           iat: Math.floor(Date.now() / 1000),
           exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
           "https://hasura.io/jwt/claims": {
             "x-hasura-allowed-roles": ["user", "admin"],
             "x-hasura-default-role": "user",
-            "x-hasura-user-id": `${metadata.issuer}`,
+            "x-hasura-user-id": `${issuer}`,
           },
         },
         process.env.JWT_SECRET
       );
 
-      const isNewUserQuery = await isNewUser(token, metadata.issuer);
-      isNewUserQuery && (await createNewUser(token, metadata));
+      const isNewUserQuery = await isNewUser(token, issuer);
+      isNewUserQuery && await createNewUser(token, metadata);
       setTokenCookie(token, res);
+
       res.send({ done: true });
     } catch (error) {
       console.error("Something went wrong logging in", error);
--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,4 +1,4 @@
-import { magicAdmin } from "../../lib/magic-Server";
+import { mAdmin } from "../../lib/magic-Server";
 import { removeTokenCookie } from "../../lib/cookie";
 import { verifyUser } from "../../lib/utils/verifyUser";
 
@@ -9,15 +9,17 @@ export default async function logout(req, res) {
     const token = req.cookies.token;
 
     const userId = await verifyUser(token);
+    console.log(userId)
     removeTokenCookie(res);
     try {
-      await magicAdmin.users.logoutByIssuer(userId);
+     await mAdmin.users.getMetadataByIssuer(issuer);
     } catch (error) {
       console.error("Error occurred while logging out magic user", error);
     }
     //redirects user to login page
     res.writeHead(302, { Location: "/login" });
     res.end();
+    return;
   } catch (error) {
     console.error({ error });
     res.status(401).json({ message: "User is not logged in" });
