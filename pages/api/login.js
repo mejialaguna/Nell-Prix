0c2f5f5e4b25825af093a5795fc57978e02e55c4
Jose Mejia <mejialaguna@yahoo.com>
2022-04-01 21:31:32 -0400

    var

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,9 +1,7 @@
-import {mAdmin} from "../../lib/magic-Server"
+import { mAdmin } from "../../lib/magic-Server";
 import jwt from "jsonwebtoken";
 import { isNewUser, createNewUser } from "../../lib/db";
 import { setTokenCookie } from "../../lib/cookie";
-
-
 
 export default async function login(req, res) {
   if (req.method === "POST") {
@@ -12,25 +10,29 @@ export default async function login(req, res) {
       const didToken = auth ? auth.substr(7) : "";
 
       const metadata = await mAdmin.users.getMetadataByToken(didToken);
+      const { issuer, email, publicAddress } = metadata;
 
       const token = jwt.sign(
         {
-          ...metadata,
+          issuer,
+          email,
+          publicAddress,
           iat: Math.floor(Date.now() / 1000),
           exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
           "https://hasura.io/jwt/claims": {
             "x-hasura-allowed-roles": ["user", "admin"],
             "x-hasura-default-role": "user",
-            "x-hasura-user-id": `${metadata.issuer}`,
+            "x-hasura-user-id": `${issuer}`,
           },
         },
         process.env.JWT_SECRET
       );
 
-      const isNewUserQuery = await isNewUser(token, metadata.issuer);
+      const isNewUserQuery = await isNewUser(token, issuer);
       isNewUserQuery && (await createNewUser(token, metadata));
       setTokenCookie(token, res);
       res.send({ done: true });
+      
     } catch (error) {
       console.error("Something went wrong logging in", error);
       res.status(500).send({ done: false });
--- pages/login/index.js
+++ pages/login/index.js
@@ -6,111 +6,128 @@ import { useState } from "react";
 import cls from "classnames";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { magicLink } from "../../lib/magic-client";
+import { magicLink } from "../../lib/magic-client/index";
 import Loading from "../../components/Loading";
 import Link from "next/link";
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [userMsg, setUserMsg] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+const regex =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
+const Login = function () {
+  const [loading, setLoading] = useState(false);
+  const [isValid, SetIsValid] = useState(false);
+  const [userMsg, setUserMsg] = useState("");
+  const [email, setEmail] = useState("");
   const router = useRouter();
 
+  function handleOnChange(e) {
+    const email = e.target.value;
+    validateEmail(email);
+    setEmail(email);
+    setUserMsg("");
+  }
+
+  function validateEmail(email) {
+    if (regex.test(email)) {
+      SetIsValid(true);
+    } else {
+      SetIsValid(false);
+    }
+  }
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    if (isValid && email) {
+      // log in a user by their email
+      setLoading(true);
+      const didToken = await magicLink.auth.loginWithMagicLink({
+        email,
+      });
+
+      if (didToken) {
+        const response = await fetch("/api/login", {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${didToken}`,
+            "Content-Type": "application/json",
+          },
+        });
+
+        const loggedInResponse = await response.json();
+        if (loggedInResponse.done) {
+          router.push("/");
+        } else {
+          setUserMsg("invalid Email address");
+          setEmail("");
+          setLoading(false);
+        }
+      }
+    } else if (!isValid) {
+      setUserMsg("invalid Email address");
+      setEmail("");
+      setLoading(false);
+    }
+  }
+
   useEffect(() => {
-    const handleComplete = () => {
-      setIsLoading(false);
-    };
+    function handleComplete() {
+      setLoading(false);
+    }
     router.events.on("routeChangeComplete", handleComplete);
     router.events.on("routeChangeError", handleComplete);
-
     return () => {
       router.events.off("routeChangeComplete", handleComplete);
       router.events.off("routeChangeError", handleComplete);
     };
   }, [router]);
 
-  const handleOnChangeEmail = (e) => {
-    setUserMsg("");
-    const email = e.target.value;
-    setEmail(email);
-  };
-
-  const handleLoginWithEmail = async (e) => {
-    e.preventDefault();
-
-    if (email) {
-      // log in a user by their email
-      try {
-        setIsLoading(true);
-
-        const didToken = await magicLink.auth.loginWithMagicLink({
-          email,
-        });
-        if (didToken) {
-          const response = await fetch("/api/login", {
-            method: "POST",
-            headers: {
-              Authorization: `Bearer ${didToken}`,
-              "Content-Type": "application/json",
-            },
-          });
-
-          const loggedInResponse = await response.json();
-          if (loggedInResponse.done) {
-            router.push("/");
-          } else {
-            setIsLoading(false);
-            setUserMsg("Something went wrong logging in");
-          }
-        }
-      } catch (error) {
-        // Handle errors if required!
-        console.error("Something went wrong logging in", error);
-        setIsLoading(false);
-      }
-    } else {
-      // show user message
-      setIsLoading(false);
-      setUserMsg("Enter a valid email address");
-    }
-  };
   return (
     <div className={styles.container}>
       <Head>
-        <title>Netflix SignIn</title>
+        <title> Netflix SignIn</title>
       </Head>
 
       <header className={styles.header}>
         <div className={styles.headerWrapper}>
-          <a className={styles.logoLink} href="/">
-            <div className={styles.logoWrapper}>
-              <Image
-                src={netflixLogo}
-                alt="Netflix logo"
-                width="128px"
-                height="34px"
-              />
-            </div>
-          </a>
+          <Link href="/">
+            <a className={styles.logoLink}>
+              <div className={styles.logoWrapper}>
+                <Image
+                  src={netflixLogo}
+                  width="128px"
+                  height="34px"
+                  alt="netflix logo"
+                />
+              </div>
+            </a>
+          </Link>
         </div>
       </header>
-
       <main className={styles.main}>
         <div className={styles.mainWrapper}>
-          <h1 className={styles.signinHeader}>Sign In</h1>
-
+          <h1 className={styles.signInHeader}> Sign In</h1>
           <input
+            onChange={handleOnChange}
+            label="Email Address"
+            name="Email Address"
+            value={email}
+            className={styles.emailInput}
             type="text"
             placeholder="Email address"
-            className={styles.emailInput}
-            onChange={handleOnChangeEmail}
           />
-
-          <p className={styles.userMsg}>{userMsg}</p>
-          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
-            {isLoading ? "Loading..." : "Sign In"}
-          </button>
+          <p className={styles.userMsg}> {userMsg}</p>
+          {loading ? (
+            <Loading />
+          ) : (
+            <button
+              onClick={handleLogin}
+              className={cls(styles.loginBtn, !email && styles.disabled)}
+              disabled={!email}
+            >
+              Sign In
+            </button>
+          )}
         </div>
       </main>
     </div>
